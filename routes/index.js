06461dedec69df7a3fb345237b8f4d84c5c01fd0
tkaneda0078 <1111510078r21@gmail.com>
2018-08-21 00:29:42 +0900

    sample osmosis

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET home page. */
+const express = require('express');
+const router = express.Router();
+const osmosis = require('osmosis');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  // sample url
+  let url = 'https://en.wikipedia.org/wiki/The_Avengers';
+  osmosis
+    .get(url)
+    .set({
+      heading: 'h1',
+      title: 'title'
+    })
+    .data(item => {
+      console.log(item);
+    })
+    .done(() => {
+    });
+
 });
 
 module.exports = router;
