d64ccd2a2f737182422c67734be7d792b557f333
tkaneda0078 <1111510078r21@gmail.com>
2018-08-21 23:25:03 +0900

    スクレイピング

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,33 @@
 'use strict';
-
 const express = require('express');
-const router = express.Router();
+const router  = express.Router();
 const osmosis = require('osmosis');
 
+const fetchPrefecturesInfo = () => {
+  return new Promise((resolve, reject) => {
+    let result = [],
+        url = 'https://www.benricho.org/chimei/latlng_data.html';
+    osmosis
+      .get(url)
+      .find('#inner tr:gt(1)')
+      .set({
+        prefecture: 'td[1] a[2]',
+        latlon: 'td[4] div:first'
+      })
+      .data(data => result.push(data))
+      .error(err => reject(err))
+      .done(() => resolve(result));
+  });
+};
+
 router.get('/', function(req, res, next) {
-
-  // sample url
-  let url = 'https://en.wikipedia.org/wiki/The_Avengers';
-  osmosis
-    .get(url)
-    .set({
-      heading: 'h1',
-      title: 'title'
-    })
-    .data(item => {
-      console.log(item);
-    })
-    .done(() => {
-    });
-
+  fetchPrefecturesInfo().then(data => {
+    console.log('--------success------');
+    console.log(data);
+  }).catch(err => {
+    console.log('--------error------');
+    console.log(err);
+  });
 });
 
 module.exports = router;
